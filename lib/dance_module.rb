35f0dca1922682c57010beaaaa029b01994e2d13
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-24 21:40:07 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -2,11 +2,11 @@ module Dance
 
   def twirl
     "I'm twirling!"
-  end 
+  end
 
   def jump
     "Look how high I'm jumping!"
-  end 
+  end
 
   def pirouette
     "I'm doing a pirouette"
@@ -14,6 +14,6 @@ module Dance
 
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
-  end 
-  
+  end
+
 end 
